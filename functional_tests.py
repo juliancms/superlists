5f522228ba62dbec7eecef522916c02f7976a064
Julián Marín <marin.julian@gmail.com>
2016-06-27 09:41:33 -0500

    Los Test Funcionales ahora prueban si podemos ingresar un to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -15,12 +15,13 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(1)
 
     def tearDown(self):
         self.browser.quit()
@@ -31,16 +32,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # El título de la página menciona la palabra 'To-Do'
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the text!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         # Se invita al usuario a ingresar un ítem para hacer inmediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # El usuario escribe "Practicar canto en inglés" en la caja de texto
+        input.send_keys('Practicar canto en inglés')
 
         # Cuando el usuario presiona enter la página se actualiza, y ahora la página lista
         # "1. Practicar canto en inglés"
+        input.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Practicar canto en inglés' for row in rows)
+        )
 
         # La página continúa mostrando una caja de texto para añadir más ítems al "To-Do".
         # El usuario ingresa "Estudiar Python"
+        self.fail('Finish the test!')
 
         # La página se actualiza de nuevo y ahora muestra ambos ítems en la lista
 
